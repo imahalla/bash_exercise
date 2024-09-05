db34d5904250b8bc1e781d382a26f9f9e17b6be0
imahalla <israel.mahalla@gmail.com>
2024-09-05 13:05:08 +0300

    fix

--- questionnaire.sh
+++ questionnaire.sh
@@ -6,13 +6,13 @@ QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
 QUESTION3="What's your favorite coding website?"
 
-echo $QUESTION1
-read NAME
+echo "$QUESTION1"
+read -r NAME
 
-echo $QUESTION2
-read LOCATION
+echo "$QUESTION2"
+read -r LOCATION
 
-echo $QUESTION3
-read WEBSITE
+echo "$QUESTION3"
+read -r WEBSITE
 
 echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
